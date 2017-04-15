59c5b7105685d1046e9cefd186966ef54a70ee62
lamhien195 <lamhien2901@gmail.com>
2017-04-16 05:51:42 +0700

    HIEN-CM1-16042017

--- ERP/ERP.Web/Content/self-writing/js/Controllers/TonCacNoiCtrl.js
+++ ERP/ERP.Web/Content/self-writing/js/Controllers/TonCacNoiCtrl.js
@@ -1,4 +1,11 @@
-﻿app.controller('TonCacNoiCtrl', function (TonCacNoiService, $scope) {
+﻿app.controller('TonCacNoiCtrl', function (TonCacNoiService, $scope, $http) {
+    var salehienthoi = $('#salehienthoi').val();
+
+    $scope.edit = function (item) {
+        $scope.ton = item;
+    };
+
+
     $scope.datatonkho = function (machuan) {
         TonCacNoiService.get_dataton(machuan).then(function (a) {
             $scope.listtonkho = a;
@@ -6,5 +13,34 @@
     };
     $scope.datatonkho('AT8');
     
+
+
+    //Thêm mới yêu cầu
+    $scope.addYeuCau = function () {
+
+        $scope.YeuCau = {
+            MA_HANG: $scope.ton.MA_HANG,
+            MA_CHUAN: $scope.ton.MA_CHUAN,
+            THONG_SO: $scope.ton.THONG_SO,
+            HANG: $scope.ton.MA_NHOM_HANG,
+            SALES_YEU_CAU: salehienthoi,
+            PUR_XU_LY: $scope.PUR_XU_LY,
+            SO_LUONG: $scope.SO_LUONG,
+            GHI_CHU: $scope.GHI_CHU
+        }
+
+        //Lưu vào CSDL
+
+        $http({
+            method: 'POST',
+            data: $scope.YeuCau,
+            url: window.location.origin + '/api/Api_YeuCauHoiGia'
+        }).then(function successCallback(response) {
+            alert('Bạn đã gửi đơn hỏi giá thành công');
+        }, function errorCallback(response) {
+            console.log(response);
+            alert('Sự cố hệ thống, Bạn vui lòng kiểm tra kết nối Internet hoặc liên hệ với admin để được hỗ trợ ');
+        });
+    }
 
 });
