87f7a1cfe805b3d5339c44e364e74513a4be0b9a
lamhien195 <lamhien2901@gmail.com>
2017-05-12 23:56:56 +0700

    hien-cm3

---
+++ ERP/ERP.Web/Areas/Inventory/Controllers/ExportExcelController.cs
@@ -0,0 +1,41 @@
+﻿using ERP.Web.Models.Database;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ERP.Web.Areas.Inventory.Controllers
+{
+    public class ExportExcelController : Controller
+    {
+        private ERP_DATABASEEntities db = new ERP_DATABASEEntities();
+        // GET: Inventory/ExportExcel
+        public ActionResult ExportPhieuXuatKho()
+        {
+            return View(db.GetAll_KhachHang());
+        }
+
+        public ActionResult ExportToExcel()
+        {
+            var gv = new GridView();
+            gv.DataSource = this.ExportPhieuXuatKho();
+            gv.DataBind();
+            Response.ClearContent();
+            Response.Buffer = true;
+            Response.AddHeader("content-disposition", "attachment; filename=DemoExcel.xls");
+            Response.ContentType = "application/ms-excel";
+            Response.Charset = "";
+            StringWriter objStringWriter = new StringWriter();
+            HtmlTextWriter objHtmlTextWriter = new HtmlTextWriter(objStringWriter);
+            gv.RenderControl(objHtmlTextWriter);
+            Response.Output.Write(objStringWriter.ToString());
+            Response.Flush();
+            Response.End();
+            return View("Index");
+        }
+    }
+}
